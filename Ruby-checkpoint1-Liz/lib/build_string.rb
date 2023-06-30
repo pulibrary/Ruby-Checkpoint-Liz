99d6d1070460825bacebe27ec5ea54ca27ffd6c3
Liz Garcia <lg6248@princeton.edu>
2023-06-30 09:56:29 -0400

    set up rspec and gemfile write test write class that returns string

---
+++ Ruby-checkpoint1-Liz/lib/build_string.rb
@@ -0,0 +1,6 @@
+# lib/string_calculator.rb
+class BuildString
+    def self.build(string)
+        string.to_s  
+    end
+end
---
+++ Ruby-checkpoint1-Liz/spec/build_string_spec.rb
@@ -0,0 +1,35 @@
+require "build_string"
+
+
+describe BuildString do
+    # Reminder: use . in front of class methods, and # in front of instance methods.
+    describe ".build" do 
+        context "given an empty string" do
+        # it keyword marks specific test case/ expected behavior
+          it "returns empty string" do
+            expect(BuildString.build("")).to eq("")
+          end
+        end
+
+        context "given non-empty string" do
+            it "returns input string" do
+
+                string = "This is the house that Jack built.
+                This is the malt that lay in the house that Jack built.
+                This is the rat that ate the malt that lay in the house that Jack built.
+                This is the cat that killed the rat that ate the malt that lay in the house that Jack built.
+                This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
+                This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
+                This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
+                This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
+                This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
+                This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
+                This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
+                This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
+                "
+                expect(BuildString.build(string)).to eq(string)
+            end
+        end
+
+    end
+end
