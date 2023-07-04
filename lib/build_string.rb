4730ae7f6e36e3dec11908283db4f045a4a16d03
Liz Garcia <lg6248@princeton.edu>
2023-07-03 20:21:43 -0400

    Works now, checked with local testing.

--- lib/build_string.rb
+++ lib/build_string.rb
@@ -5,75 +5,125 @@ class BuildString
     "This is the house that Jack built.\nThis is the malt that lay in the house that Jack built.\nThis is the rat that ate the malt that lay in the house that Jack built.\nThis is the cat that killed the rat that ate the malt that lay in the house that Jack built.\nThis is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\nThis is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\nThis is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\nThis is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\nThis is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\nThis is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\nThis is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\nThis is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built."
   end
 
-  # given content, which is a string, returns array of elements. 
+  # given a rhyme, which is a string, returns array of elements. 
   # Ex: "1- the house that Jack built, 2- the malt that lay in, etc"
-  def get_elements(content)
-    puts "content = #{content}"
+  def get_elements(string)
     # array of lines
-    lines = content.each_line.to_a
+    lines = string.each_line.to_a
 
     # elements array
     elements = []
 
+    # number of words in previous element
+    prev_length = 0
+
     lines.each do |line|
-      puts("line = #{line}")
+      # puts "line = #{line}"
       words = line.split 
-      puts "words = #{words}"
-      count = 0
-      element = String.new
-      words.each do |word|
-        puts "word = #{word}"
-        if word == "the"
-          count += 1
-        end
-        puts "count = #{count}"
-        if word != "This" && word != "is"
-          if word[-1] != '.' && !(count == 2 && word == "the")
-            # concatenate word into string variable "element"
-            element << word
-            element << " "
-          end
-        end
-        # first element of rhyme 
-        if word[-1] == '.'
-          # remove dot from end before adding word to element string
-          element << word.chop
-        end
-        puts "element = #{element}"
-        # when we encounter the second "the", break and continue with next line
-        if count == 2 && word == "the"
-          break
-        end
+      # puts "words = #{words}"
+      # count = 0
+      element = []
+      # number of words in current line
+      curr_length = words.length
+      # puts "prev_length = #{prev_length}"
+      # puts "curr_length = #{curr_length}"
+      diff = curr_length - prev_length
+      # puts "diff = #{diff}"
+
+      (diff).times do |i|
+        next if words[i] == "This" || words[i]== "is"
+        # concatenate word into element string
+        element << words[i]
+        # puts "element = #{element}"
+      end
+      prev_length += element.length
+      string = element.join(" ")
+      
+      if string[-1] == "."
+        string = string.chop
       end
 
-      # remove space from last word of a line
-      if element[-1] == " "
-        element = element.chop
-        puts "element! = #{element}"
-      end
-
-      elements.push(element)
-      puts "elements array = #{elements}"
+      elements.push(string)
+      # puts "elements array = #{elements}"
     end
     # return array of elements
-    puts "returned elements array = #{elements}"
+    # puts "returned elements array = #{elements}"
     elements
   end
 
   # given an array "elements" with all elements of the rhyme, return a string 
   # with all the elements in a random order, but the same random order on each line
-  def randomize_rhyme(elements)
+  def randomize_rhyme(rhyme)
+    elements = get_elements(rhyme)
+    puts "elements = #{elements}"
     # shuffle method does not modify the original array in place, it returns a modified version.
     shuffled_elements = elements.shuffle 
-    output = String.new
-    saved = String.new
+    puts "shuffled = #{shuffled_elements}"
+    output = ""
+    saved = ""
 
     shuffled_elements.each do |element|
-      output += "This is #{element} #{saved}.\n "
-      saved = output
+      if saved == ""
+        output += "This is #{element}.\n"
+        saved = "#{element}"
+      else
+        output += "This is #{element} #{saved}.\n "
+        saved = "#{element} #{saved}"
+      end
+      puts "output = #{output}"
     end
-    # return the resulting string of rhyme with randomized order of elements, excluding the final newline char
+    # return the resulting string of rhyme with randomized order of elements,
+    # excluding the final newline char
     output.chop
+  end
+
+  # pass a string (rhyme) and confirm that it has correct pattern
+  def validate_rhyme(string)
+    lines = string.each_line.to_a
+    saved = []
+    saved_length = 0
+
+    lines.each do |line|
+      words = line.split
+      puts "line of words = #{words}"
+
+      line_length = words.length
+      diff = line_length - saved_length
+      puts "diff = #{diff}"
+      puts "saved_length = #{saved_length}"
+
+      # check "This" and "is" are the two first words
+      if words[0] != "This" || words[1] != "is"
+        return false
+      end
+
+      # check if the last part of the line matches what we saved
+      j = 0
+      i = diff
+      while saved and i >= diff && i < line_length
+        puts "words[i] = #{words[i]}"
+        puts "saved[j] = #{saved[j]}"
+        if words[i] != saved[j] 
+          return false
+        end
+        j += 1
+        i += 1
+      end
+
+      # save middle part (new component of rhyme)
+      saved = []
+      i = 2
+        while i < line_length
+          saved << words[i]
+          i += 1
+        end
+
+      puts "saved = #{saved}"
+      saved_length = saved.length
+    end
+    # return true: pattern matched
+    true
+
   end
 end
 
@@ -83,18 +133,26 @@ puts "Testing"
 
 testing = BuildString.new
 
-string = "This is the house that Jack built.
-        This is the malt that lay in the house that Jack built.
-        This is the rat that ate the malt that lay in the house that Jack built.
-        "
+# string = "This is the man all tattered and torn that kissed.\nThis is the horse and the hound and the horn that belonged to the man all tattered and torn that kissed.\nThis is the cat that killed the horse and the hound and the horn that belonged to the man all tattered and torn that kissed."
 
-output = testing.get_elements(string)
+song = "This is the man all tattered and torn that kissed.\nThis is the princess that waved to the man all tattered and torn that kissed."
 
-puts output
-  
+# local testing for randomize_rhyme method
+randomized = testing.randomize_rhyme(song)
+puts "randomized = #{randomized}"
+
+# string = "This is the house that Jack built.
+# This is the malt that lay in the house that Jack built.
+# This is the rat the great that ate the malt that lay in the house that Jack built."
+
+# boolean = testing.validate_rhyme(randomized)
+# puts "validated: #{boolean}"
+# puts "randomized is equal to original? #{song == randomized}"
 
 # This is *the house that Jack built. --> 5 
-# This is the malt that lay in the house that Jack built. 
+# This is the malt that lay in *the house that Jack built. --> middle: 2 to 6, total of 12, prev of 5, 12-5 = 7 - 1
+                                                              # middle = 2 to length-prev.length-1
+                                                              # last = length-prev.length to length-1
 # This is the rat that ate the malt that lay in the house that Jack built.
 # This is the cat that killed the rat that ate the malt that lay in the house that Jack built.
 # This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
@@ -107,3 +165,4 @@ puts output
 # This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
 
 # 12 lines total
+
--- spec/build_string_spec.rb
+++ spec/build_string_spec.rb
@@ -1,12 +1,10 @@
-# frozen_string_literal: true
-
 require 'build_string'
 
 describe BuildString do
   subject(:jack_house) { described_class.new }
 
   # Reminder: use . in front of class methods, and # in front of instance methods.
-  describe '.rhyme' do
+  describe '#rhyme' do
     context 'jackhouse' do
       it 'returns jackhouse rhyme' do
         string = "This is the house that Jack built.\nThis is the malt that lay in the house that Jack built.\nThis is the rat that ate the malt that lay in the house that Jack built.\nThis is the cat that killed the rat that ate the malt that lay in the house that Jack built.\nThis is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\nThis is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\nThis is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\nThis is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\nThis is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\nThis is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\nThis is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\nThis is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built."
@@ -15,23 +13,10 @@ describe BuildString do
     end
   end
 
-
-  describe ".get_elements" do
-    context "given a string" do
-      it "returns array of elements in the rhyme" do
-        string = "This is the house that Jack built.
-        This is the malt that lay in the house that Jack built.
-        This is the rat that ate the malt that lay in the house that Jack built.
-        This is the cat that killed the rat that ate the malt that lay in the house that Jack built.
-        This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
-        This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
-        This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
-        This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
-        This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
-        This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
-        This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
-        This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
-        "
+  describe "#get_elements" do
+    context "given a rhyme as a string and successfully extracts elements of rhyme" do
+      it "returns true" do
+        string = "This is the house that Jack built.\nThis is the malt that lay in the house that Jack built.\nThis is the rat that ate the malt that lay in the house that Jack built.\nThis is the cat that killed the rat that ate the malt that lay in the house that Jack built.\nThis is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\nThis is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\nThis is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\nThis is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\nThis is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\nThis is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\nThis is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\nThis is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built."
 
         elements_array = ["the house that Jack built", "the malt that lay in",
         "the rat that ate", "the cat that killed", "the dog that worried", 
@@ -45,15 +30,50 @@ describe BuildString do
     end
   end
 
-  # NOTE: what would the output equal to (it's random)?
 
-  # describe ".randomize_rhyme" do
-  #   context "given an array of elements" do
-  #     it "returns a string of a randomized rhyme"
-  #       expect(jack_house.randomize_rhyme(elements_array)).to eq()
-  #     end
-  #   end
-  # end
+  describe "#validate_rhyme" do
+    context "given a valid rhyme as a string" do
+      it "returns true" do
 
+        string = "This is the man all tattered and torn that kissed.\nThis is the horse and the hound and the horn that belonged to the man all tattered and torn that kissed.\nThis is the cat that killed the horse and the hound and the horn that belonged to the man all tattered and torn that kissed."
+
+        expect(jack_house.validate_rhyme(string)).to eq(true)
+      end
+    end
+
+    context "given an invalid rhyme as a string (has 'es')" do
+      it "returns false" do
+        string = "This is the man all tattered and torn that kissed.\nThis es the horse and the hound and the horn that belonged to the man all tattered and torn that kissed.\nThis is the cat that killed the horse and the hound and the horn that belonged to the man all tattered and torn that kissed."
+        expect(jack_house.validate_rhyme(string)).to eq(false)
+      end
+    end
+
+    context "given an invalid rhyme as a string (has 'cow')" do
+      it "returns false" do
+        string = "This is the man all tattered and torn that kissed.\nThis is the cow and the hound and the horn that belonged to the man all tattered and torn that kissed.\nThis is the cat that killed the horse and the hound and the horn that belonged to the man all tattered and torn that kissed."
+        expect(jack_house.validate_rhyme(string)).to eq(false)
+      end
+    end
+
+  end
+
+  describe "#randomize_rhyme" do
+    context "given a randomized rhyme with valid patterns" do
+      it "returns true" do
+        string = "This is the man all tattered and torn that kissed.\nThis is the horse and the hound and the horn that belonged to the man all tattered and torn that kissed.\nThis is the cat that killed the horse and the hound and the horn that belonged to the man all tattered and torn that kissed."
+        randomized = jack_house.randomize_rhyme(string)
+        expect(jack_house.validate_rhyme(randomized)).to eq(true)
+      end
+    end
+ 
+    context "given a randomized rhyme with invalid patterns" do
+      it "returns false" do
+        randomized = "This is the man all tattered and torn that kissed.\nThis is the horse and the hound and the horn that belonged to the woman all tattered and torn that kissed.\nThis is the cat that killed the horse and the hound and the horn that belonged to the man all tattered and torn that kissed."        
+        expect(jack_house.validate_rhyme(randomized)).to eq(false)
+      end
+    end
+  end
 
 end
+
+ 
